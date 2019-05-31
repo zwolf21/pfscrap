ca9c72ccb987741ef037a3ffbb66611a91355ad0
profp <profp@profp.co.kr>
2019-05-31 16:36:35 +0900

    settings/enviroments 패스수정

--- pfscrap/settings/environments.py
+++ pfscrap/settings/environments.py
@@ -1,3 +1,7 @@
 import sys
+import os
 
-sys.path.append('../pfscrap')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+ENV_DIR = os.path.dirname(BASE_DIR)
+sys.path.append(BASE_DIR)
+sys.path.append(ENV_DIR)
